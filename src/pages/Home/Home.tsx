654ad0d14f4bccd30b36d1a05d690940effef988
Alfonso Puerta <apuertaj2@gmail.com>
2022-10-07 12:32:55 -0500

    Added a Data Grid component for show data of the people

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,49 @@
+import { People } from '@/data'
+import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid'
 import * as React from 'react'
+
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
-  return <div>Home</div>
+  const pageSize = 5
+  const columns: GridColDef[] = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: 'category',
+      headerName: 'Categories',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: 'company',
+      headerName: 'Company',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+  ]
+
+  return (
+    <div>
+      <DataGrid
+        rows={People}
+        columns={columns}
+        disableColumnSelector
+        disableSelectionOnClick
+        autoHeight
+        pageSize={pageSize}
+        rowsPerPageOptions={[pageSize]}
+        getRowId={(row: any) => row.id}
+      />
+    </div>
+  )
 }
 
 export default Home
